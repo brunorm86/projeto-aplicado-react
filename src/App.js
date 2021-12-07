cc7bfd85e52379593fd99fa484417bd5478955dd
thais.bertoldo <thais.bertoldo@softplan.com.br>
2021-12-06 22:18:37 -0300

    feat: aula 01

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+// import HelloWorldAbc from './components/HelloWorld/HelloWorld';
+import { HelloWorld, HelloWorld2 } from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
+      <HelloWorld />
+      <HelloWorld2 />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
